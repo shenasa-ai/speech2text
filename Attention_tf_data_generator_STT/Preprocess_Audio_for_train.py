fa7ce85e5cf0ba51225946b89eccc00bb00150e7
masoudMZB <masoudparpanchi@gmail.com>
2020-06-10 17:14:56 +0430

    commneted codes removed

--- Attention_tf_data_generator_STT/Preprocess_Audio_for_train.py
+++ Attention_tf_data_generator_STT/Preprocess_Audio_for_train.py
@@ -168,29 +168,6 @@ def spectrogram_from_file(filename, step=10, window=20, max_freq=None,
     ind = np.where(freqs <= max_freq)[0][-1] + 1
     return np.transpose(np.log(pxx[:ind, :] + eps))
 
-
-# def text_to_int_sequence(text):
-#     """ Convert text to an integer sequence """
-#     int_sequence = []
-# #     text = text.lower()
-# #     print(text)
-#     for c in text:
-# #         print(c)
-#         if c == ' ':
-#             ch = char_map['<SPACE>']
-#         else:
-#             ch = char_map[c]
-#         int_sequence.append(ch)
-#     return int_sequence
-
-# def int_sequence_to_text(int_sequence):
-#     """ Convert an integer sequence to text """
-#     text = []
-#     for c in int_sequence:
-#         ch = index_map[c]
-#         text.append(ch)
-#     return text
-
 def calc_mfcc(filename):
     dim=26
     audio, sample_rate = librosa.load(PATH + 'clips/' + filename, sr=16000, duration = 10)
